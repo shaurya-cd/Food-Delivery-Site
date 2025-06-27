c2d33deb55ff74ad878ce4eb2a9f811f52b88edc
shaurya <2447shaurya48@gmail.com>
2025-06-27 21:24:38 +0530

    solved header image problem

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
+import { assets } from '../../assets/assets'
 
 function Header() {
   return (
     <div className='header'>
-        <div className="header-contents">
+        <div className="header-contents" >
             <h2>Order your favourite food here</h2>
             <p>Craving food? Order your favorites in seconds! Would you like more variations—funny, elegant, or techy?</p>
             <button><a href="#explore-menu">View Menu</a></button>
